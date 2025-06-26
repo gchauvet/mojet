9a1aa28314ed029496e46eee6c877dc29a5b6a97
Chauvet Guillaume <gchauvet@users.noreply.github.com>
2025-06-26 18:36:31 +0200

    Additional type handlers (#4)
    * Additional type handlers
    
    * fix unit tests
    
    * documentation

--- src/test/java/pro/cyberyon/mojet/types/IntegerTypeHandlerTest.java
+++ src/test/java/pro/cyberyon/mojet/types/IntegerTypeHandlerTest.java
@@ -19,32 +19,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit test of charactere type handler
+ * Unit test of integer type handler
  *
  * @author Guillaume CHAUVET
  */
-class CharactereTypeHandlerTest {
+class IntegerTypeHandlerTest {
 
-    private final CharacterTypeHandler instance = new CharacterTypeHandler();
+    private final IntegerTypeHandler instance = new IntegerTypeHandler();
 
     @Test
     void testAccept() {
         assertFalse(instance.accept(null));
         assertFalse(instance.accept(Double.class));
-        assertTrue(instance.accept(char.class));
-        assertTrue(instance.accept(Character.class));
+        assertFalse(instance.accept(long.class));
+        assertTrue(instance.accept(Integer.class));
+        assertTrue(instance.accept(int.class));
     }
 
     @Test
     void testRead() {
-        assertEquals('0', instance.read("0", null));
-        assertEquals('t', instance.read("t", null));
+        assertEquals(1, instance.read("1", null));
+        assertEquals(1985, instance.read("1985", null));
     }
 
     @Test
     void testWrite() {
-        assertEquals("t", instance.write('t', null));
-        assertEquals("€", instance.write('€', null));
+        assertEquals("0", instance.write(0, null));
+        assertEquals("1985", instance.write(1985, null));
     }
 
 }
